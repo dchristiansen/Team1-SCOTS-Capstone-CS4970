829c582521357344e2cb7346eece5bb34a9372ce
Dylan Christiansen <dylanjay007@gmail.com>
2020-02-29 16:08:22 -0600

    Added session storage stuff Dude

---
+++ Web stuff/param.js
@@ -0,0 +1,12 @@
+function setParams() {
+  var bpm = document.getElementById("BPM").value;
+  var timeWSound = document.getElementById("timeWSound").value;
+  var cycles = document.getElementById("cycles").value;
+  var timeWOSound = document.getElementById("timeWOSound").value;
+  var feedback = document.getElementById("feedback").checked;
+  sessionStorage.setItem("bpm",bpm);
+  sessionStorage.setItem("timeWSound",timeWSound);
+  sessionStorage.setItem("cycles",cycles);
+  sessionStorage.setItem("timeWOSound",timeWOSound);
+  sessionStorage.setItem("feedback",feedback);
+}
