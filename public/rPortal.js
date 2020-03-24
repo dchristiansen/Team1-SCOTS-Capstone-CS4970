e1f4c4afe42c9ed708900f4c1b214b39f55cd9e4
Gabriel Perez <abie.perez@gmail.com>
2020-03-24 18:49:37 -0500

    Fixed rPortal.js

--- public/rPortal.js
+++ public/rPortal.js
@@ -1,31 +1,30 @@
-(function() {
-    const btnLogout = document.getElementById("btnLogout");
 
-    const adminForm = document.querySelector(".admin-action");
-    //const functions = firebase.functions();
-    btnLogout.addEventListener("click", e => {
-        firebase.auth().signOut().then(function() {
-            window.location = "index.html";
-        }).catch(function(error) {
-            console.log(error);
-        });
+const btnLogout = document.getElementById("btnLogout");
+
+const adminForm = document.querySelector(".admin-action");
+//const functions = firebase.functions();
+btnLogout.addEventListener("click", e => {
+    firebase.auth().signOut().then(function() {
+        window.location = "index.html";
+    }).catch(function(error) {
+        console.log(error);
     });
+});
 
-    adminForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const adminEmail = document.getElementById('admin-email').value;
-        const addAdminRole = firebase.functions().httpsCallable('addAdminRole');
-        addAdminRole({email: adminEmail}).then(result => {
-            console.log(result);
-            if (result.data.errorInfo == null)
-            {
-                alert(result.data.message);
-            }
-            else
-            {
-                alert(result.data.errorInfo.message);
-            }
-        });
+adminForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const adminEmail = document.getElementById('admin-email').value;
+    const addAdminRole = firebase.functions().httpsCallable('addAdminRole');
+    addAdminRole({email: adminEmail}).then(result => {
+        console.log(result);
+        if (result.data.errorInfo == null)
+        {
+            alert(result.data.message);
+        }
+        else
+        {
+            alert(result.data.errorInfo.message);
+        }
     });
+});
 
-}());
