0e351b21fd18cce1464617ca38f24c936be68570
tdburton <tdburton@unomaha.edu>
2020-03-25 17:22:37 -0500

    Implemented click on table to select assignment

--- public/userdashboard.js
+++ public/userdashboard.js
@@ -1,54 +1,80 @@
 import { getAssignmentsForUser } from "./Data.js"
-(function() {
 
-    const greeting = document.getElementById("greeting");
-    var username;
-    const btnLogout = document.getElementById("btnLogout");
-    const assignmentTable = document.querySelector("#tablebody");
 
-    btnLogout.addEventListener("click", e => {
-        firebase.auth().signOut().then(function() {
-            window.location = "index.html";
-        }).catch(function(error) {
-            console.log(error);
+const greeting = document.getElementById("greeting");
+var username;
+const btnLogout = document.getElementById("btnLogout");
+const assignmentTable = document.querySelector("#tablebody");
+
+btnLogout.addEventListener("click", e => {
+    firebase.auth().signOut().then(function() {
+        window.location = "index.html";
+    }).catch(function(error) {
+        console.log(error);
+    });
+});
+
+firebase.auth().onAuthStateChanged(async function(user) {
+    if (user) {
+        username = user.email.split("@")[0];
+        greeting.innerHTML = "Welcome, " + username + "!";
+        let authId = user.uid;
+        //TODO: If user.uid is not the primary key for the user table, query user table to get
+        //primary key. Otherwise, save autId into sessionStorage
+        sessionStorage.setItem('uid', authId);
+
+        let assignmentsArray = await getAssignmentsForUser(authId);
+        console.log(assignmentsArray.dataArray);
+
+        let newHtml="";
+
+        assignmentsArray.dataArray.forEach(assignment => {
+            let tr = document.createElement('tr');
+            tr.innerHtml = "<tr>";
+            //newHtml += "<tr>";
+            tr.innerHTML += "<td>" + assignment.data.assignmentLabel + "</td>";
+            tr.innerHTML += "<td>" + assignment.data.parameters.bpm + "</td>";
+            tr.innerHTML += "<td>" + assignment.data.parameters.soundOnTime + "</td>";
+            tr.innerHTML += "<td>" + assignment.data.parameters.soundOffTime + "</td>";
+            if(assignment.data.parameters.feedback) {
+                tr.innerHTML += "<td>On</td>";
+            } else {
+                tr.innerHTML += "<td>Off</td>";
+            }
+            tr.innerHTML += "<td>" + assignment.data.parameters.cycles + "</td>";
+            tr.innerHTML += "</tr>";
+            tr.setAttribute('data-parameters', assignment.data.assignmentLabel + "," + assignment.data.parameters.bpm + "," + assignment.data.parameters.soundOnTime + ","
+            + assignment.data.parameters.soundOffTime + "," + assignment.data.parameters.feedback + "," + assignment.data.parameters.cycles);
+            assignmentTable.appendChild(tr);
         });
-    });
 
-    firebase.auth().onAuthStateChanged(async function(user) {
-        if (user) {
-            username = user.email.split("@")[0];
-            greeting.innerHTML = "Welcome, " + username + "!";
-            let authId = user.uid;
-            //TODO: If user.uid is not the primary key for the user table, query user table to get
-            //primary key. Otherwise, save autId into sessionStorage
-            sessionStorage.setItem('uid', authId);
+        //assignmentTable.innerHTML += newHtml;
+    } else {
+        // No user is signed in.
+    }
+});
 
-            let assignmentsArray = await getAssignmentsForUser(authId);
-            console.log(assignmentsArray.dataArray);
+$("#tablebody").on("click", "tr", function(){
+    console.log($(this).data('parameters'));
+    let params = $(this).data('parameters').split(",");
+    console.log(params);
 
-            let newHtml="";
+    let assignment = params[0];
+    let bpm = params[1];
+    let timeWSound = params[2];
+    let timeWOSound = params[3];
+    let cycles = params[5];
+    let feedback = params[4];
 
-            assignmentsArray.dataArray.forEach(assignment => {
-                newHtml += "<tr>";
-                newHtml += "<td>" + assignment.data.assignmentLabel + "</td>";
-                newHtml += "<td>" + assignment.data.parameters.bpm + "</td>";
-                newHtml += "<td>" + assignment.data.parameters.soundOnTime + "</td>";
-                newHtml += "<td>" + assignment.data.parameters.soundOffTime + "</td>";
-                if(assignment.data.parameters.feedback) {
-                    newHtml += "<td>On</td>";
-                } else {
-                    newHtml += "<td>Off</td>";
-                }
-                newHtml += "<td>" + assignment.data.parameters.cycles + "</td>";
-                newHtml += "</tr>";
-            });
+    sessionStorage.setItem('aid', assignment);
+    sessionStorage.setItem("bpm",bpm);
+    sessionStorage.setItem("timeWSound",timeWSound);
+    sessionStorage.setItem("cycles",cycles);
+    sessionStorage.setItem("timeWOSound",timeWOSound);
+    sessionStorage.setItem("feedback",feedback);
 
-            assignmentTable.innerHTML += newHtml;
-        } else {
-          // No user is signed in.
-        }
-    });
-
-
-    
-}());
+    window.location = "start.html";
+});
+$("td > a").on("click", function(e){
+    e.stopPropagation();
+});
