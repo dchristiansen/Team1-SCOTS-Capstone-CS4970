41434df39617de5e91f732918d419b5d138b153b
dchristiansen <dylanjay007@gmail.com>
2020-03-25 14:39:18 -0500

    Added clickable rows in rPortal & puppteer stuff

--- public/rPortal.js
+++ public/rPortal.js
@@ -8,17 +8,28 @@ function fetchData(){ //TODO: hook this up to a database call
     return data;
 }
 
+var url = "userData.html"
+
 function populateTable(data, table){
     data.forEach(function(object) {
         var tr = document.createElement('tr');
         tr.innerHTML = '<td>' + object.user_id + '</td>' +
         '<td>' + object.most_recent_session + '</td>';
+        tr.setAttribute('data-href', url);
         table.appendChild(tr);
     });
 }
 
-var url = "userData.html"
+
 
 let table = document.querySelector("#tablebody");
 let data = fetchData();
 populateTable(data, table);
+
+$('*[data-href]').on("click",function(){
+  window.location = $(this).data('href');
+  return false;
+});
+$("td > a").on("click",function(e){
+  e.stopPropagation();
+});;
