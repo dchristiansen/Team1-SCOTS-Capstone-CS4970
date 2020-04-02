610830be03feaf0fb4ab218af5f26b2a05311d7e
tdburton <tdburton@unomaha.edu>
2020-04-01 21:02:11 -0500

    Fixed session storage async bug

--- public/game/behaviours/Timer.js
+++ public/game/behaviours/Timer.js
@@ -27,9 +27,9 @@ export default class Timer extends Base.Behavior {
     constructor(bpm, soundPhaseTime, noSoundPhaseTime, cycles) {
         super();
         this.bpm = bpm;
-        this.beatTime = 60000/bpm;
-        this.soundPhaseTime = soundPhaseTime*1000;
-        this.noSoundPhaseTime = noSoundPhaseTime*1000;
+        this.beatTime = 60000 / bpm;
+        this.soundPhaseTime = soundPhaseTime * 1000;
+        this.noSoundPhaseTime = noSoundPhaseTime * 1000;
         this.cycles = cycles;
     }
 
@@ -47,9 +47,9 @@ export default class Timer extends Base.Behavior {
 
     playBeat() {
         //Check if we're in the sound on phase
-        if(this.soundOn) {
+        if (this.soundOn) {
             //Check if volume needs to be lowered (3/4 of the way through soundPhase), if so lower it
-            if (this.currentTime >= (this.startTime + (0.75 * this.soundPhaseTime))  && this.startTime != -1) {
+            if (this.currentTime >= (this.startTime + (0.75 * this.soundPhaseTime)) && this.startTime != -1) {
                 let newVolume = this.beatSound.volume - this.volumeChange;
                 if (newVolume < 0) {
                     newVolume = 0;
@@ -60,9 +60,9 @@ export default class Timer extends Base.Behavior {
             this.beatSound.play();
         }
         //If we're in the sound off phase, check if we're not on the last cycle
-        else if(this.currentCycle != this.cycles) {
+        else if (this.currentCycle != this.cycles) {
             //If not on the last cycle, check if we need to raise volume (3/4 of the way through soundOffPhase)
-            if (this.currentTime >= (this.phaseSwitchTime + (0.75 * this.noSoundPhaseTime))  && this.startTime != -1) {
+            if (this.currentTime >= (this.phaseSwitchTime + (0.75 * this.noSoundPhaseTime)) && this.startTime != -1) {
                 let newVolume = this.beatSound.volume + this.volumeChange;
                 if (newVolume > 1) {
                     newVolume = 1;
@@ -81,7 +81,7 @@ export default class Timer extends Base.Behavior {
         //Set the start time to the most recent beat that they are trying to hit
         this.startTime = this.mostRecentBeat;
         this.phaseSwitchTime = this.startTime + this.soundPhaseTime;
-        this.endTime = this.phaseSwitchTime + this.noSoundPhaseTime + (this.beatTime/2);
+        this.endTime = this.phaseSwitchTime + this.noSoundPhaseTime + (this.beatTime / 2);
         this.tapHandler.startTime = this.startTime;
         console.log("Start time is " + this.startTime);
         return this.startTime;
@@ -90,35 +90,34 @@ export default class Timer extends Base.Behavior {
     update() {
         this.currentTime = new Date().getTime();
         //If we're in the sound phase
-        if(this.soundOn) {
-            if(this.currentTime > this.phaseSwitchTime + (this.beatTime / 2)) {
+        if (this.soundOn) {
+            if (this.currentTime > this.phaseSwitchTime + (this.beatTime / 2)) {
                 console.log("Turning sound off");
                 this.soundOn = false;
             }
-        //In the sound off phase
+            //In the sound off phase
         } else {
             //If the current time we are at is the final time
-            if(this.currentTime > this.endTime) {
-                if(!this.gameOver) {
+            if (this.currentTime > this.endTime) {
+                if (!this.gameOver) {
                     //If we are on the last cycle
-                    if(this.currentCycle == this.cycles) {
-                      this.gameOver = true;
-                      let feedback = sessionStorage.getItem('feedback');
-                      let assignmentId = sessionStorage.getItem('aid');
-                      let stringTapVersion = JSON.parse(JSON.stringify(this.tapHandler.tapDataTotal));
-                      firebase.auth().onAuthStateChanged(firebaseUser => {
-                          if(firebaseUser) {
-                              console.log(stringTapVersion);
-
-                              let sesh = createSession(assignmentId, this.bpm, this.soundPhaseTime, this.noSoundPhaseTime, this.cycles, feedback, firebaseUser.uid, stringTapVersion);
-
-                              console.log(sesh);
-                          }
-                      });
-                      sessionStorage.setItem('totalTapArray', JSON.stringify(this.tapHandler.tapDataTotal));
-                      sessionStorage.setItem('score', this.scoreCalculator.calculateScore(this.tapHandler.tapDataSoundOff, this.beatTime, this.noSoundPhaseTime, this.cycles));
-                      sessionStorage.setItem('data', JSON.stringify(this.tapHandler.tapDataSoundOff));
-                      document.location.href = "./results.html";
+                    if (this.currentCycle == this.cycles) {
+                        this.gameOver = true;
+                        let feedback = sessionStorage.getItem('feedback');
+                        let assignmentId = sessionStorage.getItem('aid');
+                        let stringTapVersion = JSON.parse(JSON.stringify(this.tapHandler.tapDataTotal));
+                        let ref = this;
+                        firebase.auth().onAuthStateChanged(async function (firebaseUser) {
+                            if (firebaseUser) {
+                                console.log(stringTapVersion);
+                                let sesh = await createSession(assignmentId, ref.bpm, ref.soundPhaseTime, ref.noSoundPhaseTime, ref.cycles, feedback, firebaseUser.uid, stringTapVersion);
+                                console.log(sesh);
+                                sessionStorage.setItem('totalTapArray', JSON.stringify(ref.tapHandler.tapDataTotal));
+                                sessionStorage.setItem('score', ref.scoreCalculator.calculateScore(ref.tapHandler.tapDataSoundOff, ref.beatTime, ref.noSoundPhaseTime, ref.cycles));
+                                sessionStorage.setItem('data', JSON.stringify(ref.tapHandler.tapDataSoundOff));
+                                document.location.href = "./results.html";
+                            }
+                        });
                     } else {
                         //Reset everything for the next cycle
                         console.log("Resetting");
