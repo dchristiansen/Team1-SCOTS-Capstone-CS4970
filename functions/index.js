11ac02d4bc76d50c470d68668d7071a87de88847
Gabriel Perez <abie.perez@gmail.com>
2020-04-04 10:50:11 -0500

    Added documentatino to node cloud function

--- functions/index.js
+++ functions/index.js
@@ -3,78 +3,92 @@ const admin = require('firebase-admin')
 
 admin.initializeApp();
 
+/*
+    addAdminRole
+    params: data, context
+    Takes in a user email, and admin user context
+    and adds the admin role to the passed in user
+*/
 exports.addAdminRole = functions.https.onCall((data, context) => {
-    // get use and add custom claim (make them an admin)
+    // If a user is not authenticated, return an error message
     if (!context.auth)
     {
         return {message: "The user is not authenticated."};
     }
 
+    // If a user is not an admin, return an error message
     if(!context.auth.token.admin)
     {
         return {message: "Only admin users can add other admins."};
     }
 
+    // Get the email of the user that you want to make an admin
     return admin.auth().getUserByEmail(data.email).then(user => {
+        // Delete that user's record from the database
         let docRef = admin.firestore().collection("users").doc(user.uid);
         docRef.delete().then(() => {
             console.log("Document successfully deleted.");
         });
+
+        // Set their custom claim to admin: true
         return admin.auth().setCustomUserClaims(user.uid, {
             admin: true
         });
     }).then(() => {
+        // Return success message
         return {
-            // message: 'Success! ${data.email} has been made an admin'
             message: 'Success! The user has been made an admin.'
         }
     }).catch(error => {
+        // Return error message
         return {message: error.message};
     });
 });
 
+/*
+    createUser
+    params: data, context
+    Takes in a new user email, new user password, and admin user context
+    and creates a new user based on the passed in email and password
+
+*/
 exports.createUser = functions.https.onCall(async (data, context) => {
     try {
+
+        // If the user is not authenticated, return an error message
         if(!context.auth) {
             return {message: "The user is not authenticated."};
-            //throw new UnauthenticatedError("The user is not authenticated.");
         }
 
-        const callingUid = context.auth.uid;
-        const callingUserRecord = await admin.auth().getUser(callingUid);
-
+        // If the user is not an admin, return an error message
         if(!context.auth.token.admin) {
             return {message: "Only admin users can create new users."};
-            //throw new NotAnAdminError("Only Admin users can create new users.");
         }
 
+        // Construct a new user object
         const newUser = {
             email: data.email,
             password: data.password
         };
 
+        // Create a new user using the admin API
         const userRecord = await admin.auth().createUser(newUser);
         const userUID = userRecord.uid;
         const userID = data.email.split("@")[0];
 
+        // Construct a new database record object
         const dbRecord = {
             userID: userID,
             changePassword: false
         };
         
+        // Create a new document in the users collection with the document
+        // ID as the user uid
         await admin.firestore().collection("users").doc(userUID).set(dbRecord);
-        //return {result: 'Success! ${data.email} has been created'}
+        // Return success message
         return {message: 'Success! The user has been created.'}
     } catch (error) {
-        /*
-        if (error.type === 'UnauthenticatedError') {
-            throw new functions.https.HttpsError('unauthenticated', error.message);
-        } else if (error.type === 'NotAnAdminError' || error.type === 'InvalidRoleError') {
-            throw new functions.https.HttpsError('failed-precondition', error.message);
-        } else {
-            throw new functions.https.HttpsError('internal', error.message);
-        }
-        */
+        // Return error message
        return {message: error.message};
     }
 });
