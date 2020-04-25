35b488c60ec3e1e79a6fd6121094da3846250eda
Gabriel Perez <abie.perez@gmail.com>
2020-04-25 17:48:43 -0500

    Prevent access to userdashboard if user is an admin

--- public/user/userdashboard.js
+++ public/user/userdashboard.js
@@ -16,40 +16,54 @@ btnLogout.addEventListener("click", e => {
 
 firebase.auth().onAuthStateChanged(async function(user) {
     if (user) {
-        username = user.email.split("@")[0];
-        greeting.innerHTML = "Welcome, " + username + "!";
-        let authId = user.uid;
-        //TODO: If user.uid is not the primary key for the user table, query user table to get
-        //primary key. Otherwise, save autId into sessionStorage
-        sessionStorage.setItem('uid', authId);
+        
+        // Get admin token result
+        user.getIdTokenResult().then(async(idTokenResult) => {
+            user.admin = idTokenResult.claims.admin;
+            if(!user.admin)
+            {
+                username = user.email.split("@")[0];
+                greeting.innerHTML = "Welcome, " + username + "!";
+                let authId = user.uid;
+                //TODO: If user.uid is not the primary key for the user table, query user table to get
+                //primary key. Otherwise, save autId into sessionStorage
+                sessionStorage.setItem('uid', authId);
 
-        //Get all assignments for the current user
-        let assignmentsArray = await getAssignmentsForUser(authId);
-        //console.log(assignmentsArray.dataArray);
+                //Get all assignments for the current user
+                let assignmentsArray = await getAssignmentsForUser(authId);
+                //console.log(assignmentsArray.dataArray);
 
-        //For each assignment fetched, add the corresponding row
-        assignmentsArray.dataArray.forEach(assignment => {
-            let tr = document.createElement('tr');
-            tr.innerHtml = "<tr>";
-            tr.innerHTML += "<td>" + assignment.data.assignmentLabel + "</td>";
-            tr.innerHTML += "<td>" + assignment.data.parameters.bpm + "</td>";
-            tr.innerHTML += "<td>" + assignment.data.parameters.soundOnTime + "</td>";
-            tr.innerHTML += "<td>" + assignment.data.parameters.soundOffTime + "</td>";
-            if(assignment.data.parameters.feedback) {
-                tr.innerHTML += "<td>On</td>";
-            } else {
-                tr.innerHTML += "<td>Off</td>";
+                //For each assignment fetched, add the corresponding row
+                assignmentsArray.dataArray.forEach(assignment => {
+                    let tr = document.createElement('tr');
+                    tr.innerHtml = "<tr>";
+                    tr.innerHTML += "<td>" + assignment.data.assignmentLabel + "</td>";
+                    tr.innerHTML += "<td>" + assignment.data.parameters.bpm + "</td>";
+                    tr.innerHTML += "<td>" + assignment.data.parameters.soundOnTime + "</td>";
+                    tr.innerHTML += "<td>" + assignment.data.parameters.soundOffTime + "</td>";
+                    if(assignment.data.parameters.feedback) {
+                        tr.innerHTML += "<td>On</td>";
+                    } else {
+                        tr.innerHTML += "<td>Off</td>";
+                    }
+                    tr.innerHTML += "<td>" + assignment.data.parameters.cycles + "</td>";
+                    tr.innerHTML += "</tr>";
+
+                    //Set the parameter attribute to the values within the table
+                    tr.setAttribute('data-parameters', assignment.data.assignmentLabel + "," + assignment.data.parameters.bpm + "," + assignment.data.parameters.soundOnTime + ","
+                    + assignment.data.parameters.soundOffTime + "," + assignment.data.parameters.feedback + "," + assignment.data.parameters.cycles);
+
+                    //Append the row to the table
+                    assignmentTable.appendChild(tr);
+                });
             }
-            tr.innerHTML += "<td>" + assignment.data.parameters.cycles + "</td>";
-            tr.innerHTML += "</tr>";
-
-            //Set the parameter attribute to the values within the table
-            tr.setAttribute('data-parameters', assignment.data.assignmentLabel + "," + assignment.data.parameters.bpm + "," + assignment.data.parameters.soundOnTime + ","
-            + assignment.data.parameters.soundOffTime + "," + assignment.data.parameters.feedback + "," + assignment.data.parameters.cycles);
-
-            //Append the row to the table
-            assignmentTable.appendChild(tr);
+            else 
+            {
+                alert("You do not have access to the User Dashboard");
+                window.location = "/researcher/rPortal.html";
+            }
         });
+        
 
     } else {
         // No user is signed in.
